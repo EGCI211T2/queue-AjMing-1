e121e4c45f327d72e2d170a169ec5155b89ef16d
Sam <sampadashrestha315@gmail.com>
2026-03-15 05:21:25 +0000

    removed unnecessary messages

--- main.cpp
+++ main.cpp
@@ -37,15 +37,6 @@ int main(int argc , char **argv) {
         else {
 
            /* 2.MODIFIED enqueue here to take in 2 info at a time*/
-          /* make a loop till the argc ends 
-           make sure we get both the order and the quantity
-           put it in the queue/ join nodes
-           use the getprice funtion to return the price to the user*/
-
-          /* if (atoi(argv[i])!=1 || atoi(argv[i])!=2 || atoi(argv[i])!=3){
-            cout<<"We don't have that order. You don't have to pay."<<endl;
-           }*/
-           
             q.enqueue(atoi(argv[i]),atoi(argv[i+1]));
             i++;
         }
